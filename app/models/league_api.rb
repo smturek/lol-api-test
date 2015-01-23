81da90cb0563b01e55546d846907df4245f4e1a5
smturek <smturek@gmail.com>
2015-01-22 18:03:29 -0700

    secret keys

--- app/models/league_api.rb
+++ app/models/league_api.rb
@@ -6,7 +6,7 @@ class LeagueAPI
 
   def champions
     response = @conn.get do |req|
-      req.url "/api/lol/static-data/na/v1.2/champion?api_key="
+      req.url "/api/lol/static-data/na/v1.2/champion?api_key=#{ENV['KEY']}"
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body, symbolize_names: true)
@@ -14,7 +14,7 @@ class LeagueAPI
 
   def champion(id)
     response = @conn.get do |req|
-      req.url "/api/lol/static-data/na/v1.2/champion/#{id}?champData=all&api_key="
+      req.url "/api/lol/static-data/na/v1.2/champion/#{id}?champData=all&api_key=#{ENV['KEY']}"
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body, symbolize_names: true)
@@ -22,7 +22,7 @@ class LeagueAPI
 
   def items
     response = @conn.get do |req|
-      req.url "/api/lol/static-data/na/v1.2/item?api_key="
+      req.url "/api/lol/static-data/na/v1.2/item?api_key=#{ENV['KEY']}"
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body, symbolize_names: true)
@@ -30,7 +30,7 @@ class LeagueAPI
 
   def item(id)
     response = @conn.get do |req|
-      req.url "/api/lol/static-data/na/v1.2/item/#{id}?champData=all&api_key="
+      req.url "/api/lol/static-data/na/v1.2/item/#{id}?champData=all&api_key=#{ENV['KEY']}"
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body, symbolize_names: true)
